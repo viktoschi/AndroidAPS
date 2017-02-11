18ba09c4d22306f3bde1a8393f92ea203f0480e2
viktoria <leonieneon@gmail.com>
2017-02-11 04:24:37 +0100

    Deprecation SlidingTabLayout

--- app/src/main/java/info/nightscout/androidaps/tabs/SlidingTabLayout.java
+++ app/src/main/java/info/nightscout/androidaps/tabs/SlidingTabLayout.java
@@ -151,7 +151,7 @@ public class SlidingTabLayout extends HorizontalScrollView {
 
         mViewPager = viewPager;
         if (viewPager != null) {
-            viewPager.setOnPageChangeListener(new InternalViewPagerListener());
+            viewPager.addOnPageChangeListener(new InternalViewPagerListener());
             populateTabStrip();
         }
     }
